eabfe0cada5f7186311a06844d6062289b98b065
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-20 00:15:27 -0300

    Adiciona express e nodemon

--- index.js
+++ index.js
@@ -1,42 +1,13 @@
-const http = require('node:http')
+import express from 'express'
 
-const getHome = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.write('<h1>Hello World</h1>')
-  response.end()
-};
+const app = express()
 
-getBatatinha = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.write('<h1>Batatinha</h1>')
-  response.end()
-};
-
-getSeuTio = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.write(JSON.stringify({ nome: 'Seu Tio' }))
-  response.end()
-};
-
-const routes = {
-  '/': (req, res) => getHome(req, res),
-  '/batatinha': (req, res) => getBatatinha(req, res),
-  '/seu-tio': (req, res) => getSeuTio(req, res)
-}
-
-const router = (request, response) => {
-  const path = request.url
-  if (path in routes) {
-    routes[path](request, response)
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' })
-    response.write('<h1>404 Not Found</h1>')
-    response.end()
-  }
-}
-
-const server = http.createServer((request, response) => {
-  router(request, response)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
 })
 
-server.listen(3000, () => console.log('Estou rodando!!! Porta 3000'))
+app.get('/batatinha', (req, res) => {
+  res.status(200).json({ nome: 'Batatinha' })
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
