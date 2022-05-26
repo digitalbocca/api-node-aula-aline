30e34a623abf937e8cf382e3ee85b83b79cc5526
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-25 23:26:37 -0300

    Cria /usuarios

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+import crypto from 'crypto'
 import express from 'express'
 
 const app = express()
+
+app.use(express.json())
+
+const listaDeNomes = ['João', 'Maria', 'José']
+const listaDeSobrenomes = ['Silva', 'Santos', 'Oliveira']
+
+const sorteio = lista => lista[Math.floor(Math.random() * lista.length)]
+
+const usuarios = []
+
+// crypto.randomUUID()
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -8,9 +20,69 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/batatinha', (req, res) => {
+// RECURSO USUARIOS
+// LISTAR TODOS OS USUÁRIOS
+app.get('/usuarios', (req, res) => {
   res.status(200).json({
-    nome: 'Batatinha'
+    usuarios
+  })
+})
+
+// LISTAR UM USUÁRIO
+app.get('/usuarios/:id', (req, res) => {
+  const id = req.params.id
+  const usuario = usuarios.find(u => u.id === id)
+  res.status(200).json({
+    usuario
+  })
+})
+
+// CRIAR UM USUÁRIO
+app.post('/usuarios', (req, res) => {
+  const nome = req.body.nome
+  const sobrenome = req.body.sobrenome
+
+  const novoUsuario = {
+    nome,
+    sobrenome,
+    id: crypto.randomUUID()
+  }
+
+  usuarios.push(novoUsuario)
+
+  res.status(201).json({
+    usuario: novoUsuario
+  })
+})
+
+// EDITAR UM USUÁRIO
+app.put('/usuarios/:id', (req, res) => {
+  const id = req.params.id
+  const usuario = usuarios.find(u => u.id === id)
+
+  const usuarioEditado = {
+    ...usuario,
+    lastEdit: new Date()
+  }
+
+  const posicaoNoArray = usuarios.indexOf(usuario)
+  usuarios[posicaoNoArray] = usuarioEditado
+
+  res.status(200).json({
+    usuarioEditado
+  })
+})
+
+// APAGAR UM USUÁRIO
+app.delete('/usuarios/:id', (req, res) => {
+  const id = req.params.id
+  const usuario = usuarios.find(u => u.id === id)
+  const posicaoNoArray = usuarios.indexOf(usuario)
+
+  usuarios.splice(posicaoNoArray, 1)
+
+  res.status(200).json({
+    mensagem: 'Usuário apagado com sucesso'
   })
 })
 
